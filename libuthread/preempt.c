8fc1997a9f55e26d39c3671c3a9f9656a479468a
KevinTai01 <77222051+KevinTai01@users.noreply.github.com>
2021-02-12 22:05:12 -0800

    Update preempt.c

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -1,3 +1,4 @@
+
 #include <signal.h>
 #include <stdbool.h>
 #include <stddef.h>
@@ -15,23 +16,65 @@
  */
 #define HZ 100
 
+extern void uthread_intr_proc(void) ;
+
+static struct sigaction sigact ;
+static sigset_t         orig_sigmask ;
+
+static struct itimerval   utimer ;
+
 void preempt_start(void)
 {
-	/* TODO */
+    int timer_ret ;
+    int irt_itval_ret ;
+    
+    sigact.sa_sigaction = &uthread_intr_proc ;
+    sigact.sa_flags     = SA_RESTART ;
+
+    utimer.it_value.tv_sec   = 0 ;
+    utimer.it_value.tv_usec  = 10 ;
+    utimer.it_interval = utimer.it_value ;
+    
+    if (setitimer(ITIMER_VIRTUAL, &utimer, NULL) != 0) {
+	printf("Setting timer failed !") ;
+    }
+    
+    sigprocmask(SIG_UNBLOCK, NULL, &orig_sigmask) ;
+    if (sigaction(SIGVTALRM, &sigact, NULL) != 0) {
+	printf("Registering sigact setup failed !") ;
+    }
+
 }
 
 void preempt_stop(void)
 {
-	/* TODO */
+    sigact.sa_mask = orig_sigmask ;
+    if (sigaction(SIGVTALRM, &sigact, NULL) != 0) {
+	printf("Registering sigact setup failed !") ;
+    }
+    printf("Unset intr mask\n") ;
+
+    utimer.it_value.tv_sec   = 0 ;
+    utimer.it_value.tv_usec  = 0 ;
+    utimer.it_interval = utimer.it_value ;
+    
+    if (setitimer(ITIMER_VIRTUAL, &utimer, NULL) != 0) {
+	printf("UnSetting timer failed !") ;
+    }
 }
 
 void preempt_enable(void)
 {
-	/* TODO */
+    sigaddset(&sigact.sa_mask, SIGVTALRM) ;
+    if (sigaction(SIGVTALRM, &sigact, NULL) != 0) {
+	printf("preempt_enable failed") ;
+    }
+       
 }
 
 void preempt_disable(void)
 {
-	/* TODO */
+    if (sigaction(SIGVTALRM, &sigact, NULL) != 0) {
+	printf("preempt_enable failed") ;
+    }   
 }
-
