ebd65ccd382deae14c11b8a11ba56c244765b72b
KevinTai01 <77222051+KevinTai01@users.noreply.github.com>
2021-02-12 23:50:43 -0800

    Update preempt.c

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -15,23 +15,75 @@
  */
 #define HZ 100
 
+extern void uthread_intr_proc(int) ;
+
+static struct sigaction sigact ;
+static sigset_t         orig_sigmask ;
+
+static struct itimerval   utimer ;
+
 void preempt_start(void)
 {
-	/* TODO */
+
+    // setup signal masks
+    sigact.sa_handler = uthread_intr_proc ;
+    sigact.sa_flags = 0 ;
+    //    sigact.sa_flags     = SA_RESTART ;
+
+    // setup timers
+    utimer.it_value.tv_sec   = 0 ;
+    utimer.it_value.tv_usec  = 10000 ; // microsec to sec
+    //  utimer.it_value.tv_usec  = 1000000 / HZ ; // microsec to sec
+    utimer.it_interval = utimer.it_value ;
+
+    // register timer
+    if (setitimer(ITIMER_VIRTUAL, &utimer, NULL) != 0) {
+	printf("Setting timer failed !") ;
+    }
+
+    // register signal handler
+    sigemptyset(&sigact.sa_mask) ;
+    if (sigaction(SIGVTALRM, &sigact, NULL) != 0) {
+	printf("Registering sigact setup failed !") ;
+    }
+
 }
 
 void preempt_stop(void)
 {
-	/* TODO */
+
+    // unset signal mask
+    sigact.sa_mask = orig_sigmask ;
+    if (sigaction(SIGVTALRM, &sigact, NULL) != 0) {
+	printf("Registering sigact setup failed !") ;
+    }
+    printf("Unset intr mask\n") ;
+
+    // disable timer
+    utimer.it_value.tv_sec   = 0 ;
+    utimer.it_value.tv_usec  = 0 ;
+    utimer.it_interval = utimer.it_value ;
+    
+    if (setitimer(ITIMER_VIRTUAL, &utimer, NULL) != 0) {
+	printf("UnSetting timer failed !") ;
+    }
 }
 
 void preempt_enable(void)
 {
-	/* TODO */
+    // set mask to enable mask
+    sigaddset(&sigact.sa_mask, SIGVTALRM) ;
+    if (sigaction(SIGVTALRM, &sigact, NULL) != 0) {
+	printf("preempt_enable failed") ;
+    }
+       
 }
 
 void preempt_disable(void)
 {
-	/* TODO */
+    // set mask to ignore signal
+    //   sigact.sa_mask = orig_sigmask ;
+    if (sigaction(SIGVTALRM, &sigact, NULL) != 0) {
+	printf("preempt_enable failed") ;
+    }   
 }
-
